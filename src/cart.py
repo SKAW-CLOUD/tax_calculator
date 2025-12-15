dd84f52b04d5862ac3a1f6105d541a737bbd8d3e
SKAW-CLOUD <simokamdemabdoul@gmail.com>
2025-12-15 15:52:10 +0100

    Update cart.py

--- src/cart.py
+++ src/cart.py
@@ -9,7 +9,7 @@ def apply_discount(price, discount_percentage):
     # BUG: The previous developer subtracted the percentage number directly!
     # If price is 100 and discount is 20, this returns 80 (Correct accidentally).
     # But if price is 50 and discount is 20, this returns 30 (WRONG! Should be 40).
-    final_price = price - discount_percentage 
+   final_price = price - (price * (discount / 100))
     
     if final_price < 0:
         return 0.0
@@ -21,4 +21,4 @@ def calculate_tax(subtotal, tax_rate):
     TODO: Implement this function.
     """
     # Currently returns 0, which is illegal!
-    return 0.0
+    return subtotal*tax_rate
